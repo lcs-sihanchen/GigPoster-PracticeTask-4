e513e37f433ec772cebc2fd8ba01734962c81fbf
Chen Sihan <schen@lcs.on.ca>
2020-02-27 15:54:48 -0500

    gridlines + first square

--- Gig Poster - Practice - 4.playground/Pages/The Clash.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 4.playground/Pages/The Clash.xcplaygroundpage/Contents.swift
@@ -3,6 +3,7 @@
 import Cocoa
 import PlaygroundSupport
 
+
 /*:
  Each image is exactly 400 pixels wide by 600 pixels tall. 
  
@@ -32,6 +33,52 @@ let offWhite = Color(hue: 208, saturation: 1, brightness: 88, alpha: 100)
 let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
 
 // Begin your work below...
+
+// Background
+canvas.fillColor = orange
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+// grid lines
+
+canvas.lineColor = black
+for a in 1...7 {
+    canvas.drawLine(from: Point(x: 50*a, y: 0), to: Point(x: 50*a, y: 600))
+}
+
+for b in 1...11 {
+    canvas.drawLine(from: Point(x: 0, y: 50*b), to: Point(x: 400, y: 50*b))
+}
+
+// White
+canvas.drawShapesWithFill = false
+canvas.borderColor = offWhite
+canvas.translate(to: Point(x: 230, y: 160))
+canvas.rotate(by: 45)
+
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 300, height: 300, borderWidth: 15)
+
+
+
+
+//for y in stride(from: 90, to: 180, by: 45){
+//    canvas.rotate(by: Degrees(y))
+//for n in stride(from: 140, to: 20, by: -10){
+//
+//    if n % 20 == 0 {
+//        canvas.fillColor = offWhite
+//        canvas.drawRectangle(at: Point(x: 0, y: 0), width: n, height: n)
+//    } else if n % 20 == 10 {
+//        canvas.fillColor = orange
+//        canvas.drawRectangle(at: Point(x: 0, y: 0), width: n, height: n)
+//    }
+//
+//
+//}
+//}
+
+// gridlines
+
+
 
 
 /*:
